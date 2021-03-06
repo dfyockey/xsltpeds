ad8c86842ea5363419417902e41132361b7fdafe
dfyockey <git@diffengine.net>
2021-03-06 16:28:55 -0500

    Added GNU GPL copyright notice to source files

--- src/Procf.hpp
+++ src/Procf.hpp
@@ -1,8 +1,22 @@
 /*
  * Procf.hpp
  *
- *  Created on: Oct 28, 2020
- *      Author: David Yockey
+ *  Copyright © 2020-2021 David Yockey
+ *
+ *  This file is part of Xsltpeds.
+ *
+ *  Xsltpeds is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Xsltpeds is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Xsltpeds.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef PROCF_HPP_
